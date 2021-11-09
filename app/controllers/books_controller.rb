295c75809933c70c35d226547031b608f2c6c7f2
EC2 Default User <ec2-user@ip-172-31-27-204.ec2.internal>
2021-11-09 05:31:16 +0000

    [Finish]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+
   end
 
   def show
@@ -8,22 +10,36 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'successfully'
+    else
+      flash.now[:danger] = "要件を満たしていません。"
+      render action: :new
+    end
   end
 
   def edit
+     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'successfully'
+    else
+      render action: :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: 'successfully'
   end
 
   private
